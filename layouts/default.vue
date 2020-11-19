ca4fc5d669e768b00eb8c5804a9cb014cbc2df03
Kazuyuki Miyake <k-miyake@zengeeks.io>
2020-11-19 20:29:57 +0900

    add disclaimer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,17 @@
   <div>
     <Nuxt class="page" />
     <footer class="footer">
-      &copy; 2020 Microsoft
+      <div class="footer__content">
+        <div class="disclaimer">
+          当社は、外部のリンク先ウェブサイトの内容に関していかなる責任も負うものではありません。お客様は、自らの責任においてこれらのリンクをご利用ください。なお、お客様によるリンクご利用の結果、ないしはリンクご利用に関連して、お客様が被るいかなる損害または損失について当社は、責任を負うものではありません。
+        </div>
+      </div>
+      <div class="footer__content">
+        <a href="https://github.com/microsoft/code-labs-jp" target="_blank">https://github.com/microsoft/code-labs-jp</a>
+      </div>
+      <div class="footer__content">
+        &copy; 2020 Microsoft
+      </div>     
     </footer>
   </div>
 </template>
@@ -58,14 +68,26 @@ button {
 
 <style scoped>
 .footer {
-  height: var(--footer-height);
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: #000;
   text-align: center;
   color: var(--color-secondary-light);
   font-size: 0.8rem;
+  padding: 20px 0;
+}
+
+.footer__content {
+  width: 90vw;
+  max-width: 70rem;
+}
+
+.disclaimer {
+  text-align: left;
+  font-size: 0.7rem;
+  margin-bottom: 10px;
 }
 
 .page {
